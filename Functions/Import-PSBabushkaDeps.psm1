ed1c31075c5c048dd34733e54ec5028444659646
MattPlacek <dp185133@ncr.com>
2017-10-12 14:03:35 -0400

    Minor tweaks to get this in a working state; add use of $ENV:PATH_BABUSHKA

--- Functions/Import-PSBabushkaDeps.psm1
+++ Functions/Import-PSBabushkaDeps.psm1
@@ -1,12 +1,11 @@
+# --*-Powershell-*--
 Import-Module "$PSScriptRoot\New-PSBabushkaDep.psm1" -Force
 
 Function Import-PSBabushkaDeps {
   Param (
-    [Parameter(Mandatory=$True)] [String] $From,
-    [Parameter(Mandatory=$True)] [Ref]    $Into
+    [Parameter(Mandatory=$True)] [String] $From
   )
 
-  $PSBabushkaDepDirs = Get-ChildItem -Path $From -Directory -Include 'PSBabushkaDeps' -Recurse
-  $PSBabushkaDepFiles = $PSBabushkaDepDirs | ForEach-Object { Get-ChildItem -Path $_.FullName -File -Include "*.Dep.ps1" -Recurse }
-  $Into.Value.Deps = $PSBabushkaDepFiles | ForEach-Object { Invoke-Expression $_.FullName }
+  $PSBabushkaDepFiles = Get-ChildItem -Path $From -File -Include "*.Dep.ps1" -Recurse
+  $PSBabushkaDepFiles | ForEach-Object { Invoke-Expression $_.FullName }
 }
--- Functions/Invoke-PSBabushka.psm1
+++ Functions/Invoke-PSBabushka.psm1
@@ -1,3 +1,4 @@
+# --*-Powershell-*--
 Import-Module "$PSScriptRoot\Import-PSBabushkaDeps.psm1" -Force
 Import-Module "$PSScriptRoot\Select-PSBabushkaDep.psm1" -Force
 Import-Module "$PSScriptRoot\Invoke-PSBabushkaDep.psm1" -Force
@@ -5,10 +6,13 @@ Import-Module "$PSScriptRoot\Invoke-PSBabushkaDep.psm1" -Force
 Function Invoke-PSBabushka {
   Param (
     [Parameter(Mandatory=$True)]  [String] $Name,
-    [Parameter(Mandatory=$False)] [String] $Path = (Get-Location)
+    [Parameter(Mandatory=$False)] [String] $Path
   )
 
-  Import-PSBabushkaDeps -From $Path -Into ([Ref] $PSBabushka)
+  if (-not $Path) { $Path = $env:PATH_BABUSHKA }
+  if (-not $Path) { $Path = (Get-Location) }
+
+  $PSBabushka.Deps = Import-PSBabushkaDeps -From $Path
   $PSBabushka.Dep = Select-PSBabushkaDep -Name $Name
   Invoke-PSBabushkaDep $PSBabushka.Dep
 }
